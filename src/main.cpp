3f1770a83bc1a15e7a6c4b904c13c20ec161671a
Dra9on123 <sernaedward1@gmail.com>
2025-04-13 13:32:27 -0500

    unstable

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,39 @@
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 #include <GLFW/glfw3.h>
+#define GL_SILENCE_DEPRECATION
+#include <stdio.h>
+#include<iostream>
 
 int main() {
     glfwInit();
-    GLFWwindow* window = glfwCreateWindow(500, 200, "ImGui App", nullptr, nullptr);
+
+    // Fullscreen
+ //   int count;
+ //   const int monitorNum = 1;
+ //   GLFWmonitor** monitor = glfwGetMonitors(&count);
+ //   const GLFWvidmode* mode = glfwGetVideoMode(monitor[monitorNum]);
+	//GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "ImGui App", monitor[monitorNum], NULL);
+
+    GLFWwindow* window = glfwCreateWindow(1500, 800, "ImGui App", nullptr, nullptr);
+
+    if (window == NULL){
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
     glfwMakeContextCurrent(window);
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+
+    ImGui::StyleColorsDark();
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 130");
@@ -26,11 +50,14 @@ int main() {
         ImGui::Text("This is some text.");
         ImGui::End();
 
+        glClearColor(0.13f, 0.13f, 0.13f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
         ImGui::Render();
         int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
-        glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(window);
