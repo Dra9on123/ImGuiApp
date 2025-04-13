42799a2403da7b3648367f5d922b0f71c5f97132
Dra9on123 <sernaedward1@gmail.com>
2025-04-13 13:40:30 -0500

    Added the docking feature of the next branch in imgui! [Stable]

--- src/main.cpp
+++ src/main.cpp
@@ -2,42 +2,38 @@
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 #include <GLFW/glfw3.h>
-#define GL_SILENCE_DEPRECATION
+
 #include <stdio.h>
-#include<iostream>
+#include <iostream>
 
 int main() {
-    glfwInit();
-
-    // Fullscreen
- //   int count;
- //   const int monitorNum = 1;
- //   GLFWmonitor** monitor = glfwGetMonitors(&count);
- //   const GLFWvidmode* mode = glfwGetVideoMode(monitor[monitorNum]);
-	//GLFWwindow* window = glfwCreateWindow(mode->width, mode->height, "ImGui App", monitor[monitorNum], NULL);
-
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW\n";
+        return -1;
+    }
     GLFWwindow* window = glfwCreateWindow(1500, 800, "ImGui App", nullptr, nullptr);
-
-    if (window == NULL){
-        std::cout << "Failed to create GLFW window" << std::endl;
+    if (window == nullptr) {
+        std::cerr << "Failed to create GLFW window\n";
         glfwTerminate();
         return -1;
     }
+
     glfwMakeContextCurrent(window);
-    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
-    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
     ImGui::StyleColorsDark();
 
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 130");
+
 
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -50,15 +46,20 @@ int main() {
         ImGui::Text("This is some text.");
         ImGui::End();
 
-        glClearColor(0.13f, 0.13f, 0.13f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-
         ImGui::Render();
         int display_w, display_h;
         glfwGetFramebufferSize(window, &display_w, &display_h);
         glViewport(0, 0, display_w, display_h);
+        glClearColor(0.13f, 0.13f, 0.13f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+            GLFWwindow* backup_context = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backup_context);
+        }
 
         glfwSwapBuffers(window);
     }
@@ -68,4 +69,6 @@ int main() {
     ImGui::DestroyContext();
     glfwDestroyWindow(window);
     glfwTerminate();
+
+    return 0;
 }
